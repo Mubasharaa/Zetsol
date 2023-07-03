162e615804ebb4291a5b52be87513d67fdbddf99
Mubasharaa <64917130+Mubasharaa@users.noreply.github.com>
2023-07-03 16:53:17 +0500

    Task 1
    Task 1 complete

---
+++ Employee-ID_Card_Task3/config.php
@@ -0,0 +1,11 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "employee_id_card";
+
+// Create a connection
+$connection = mysqli_connect($hostname, $username, $password, $database);
+
+// Die if connection was not successful
+?>
